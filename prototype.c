b7ddcc464ad862588e0fa8c132f75e7f92f29ec4
Scorch879 <148331130+Scorch879@users.noreply.github.com>
2025-03-24 16:38:08 +0800

    Update prototype.c

--- prototype.c
+++ prototype.c
@@ -6,20 +6,19 @@
 #include <time.h>
 
 // Game constants
-#define WIDTH 50
+#define WIDTH 60
 #define HEIGHT 25
-#define CAR_WIDTH 1
-#define OBSTACLE_WIDTH 3
-#define BOSS_WIDTH 9
-#define LANE_COUNT 3
+#define CAR_WIDTH 3
+#define OBSTACLE_WIDTH 5
+#define BOSS_WIDTH 11
+#define LANE_COUNT 4
 #define LANE_WIDTH (WIDTH / (LANE_COUNT + 1))
-#define MAX_OBSTACLES 10
 #define INITIAL_LIVES 3
 #define BASE_SPEED 80
 #define MIN_SPEED 20
-#define SLOWMO_DURATION 50  // ticks
+#define SLOWMO_DURATION 50
 
-int i = 0;
+
 int score = 0;
 int carLane = 1;
 bool gameOver = false;
@@ -31,18 +30,24 @@ bool slowmo = false;
 int slowmoTicks = 0;
 int highScore = 0;
 
-void gameLoop();
-void restartGame();
-HANDLE hConsole; // Console handle for colors
 
-typedef struct {
+int i, j, l;
+
+HANDLE hConsole;
+
+
+typedef struct ObstacleNode {
     int lane;
     float y;
-    bool active;
     bool boss;
-} Obstacle;
+    struct ObstacleNode* next;
+} ObstacleNode;
 
-Obstacle obstacles[MAX_OBSTACLES];
+ObstacleNode* obstacleHead = NULL;
+
+
+void restartGame();
+void gameLoop();
 
 void setColor(int color) {
     SetConsoleTextAttribute(hConsole, color);
@@ -54,14 +59,11 @@ void gotoxy(int x, int y) {
 }
 
 void hideCursor() {
-    CONSOLE_CURSOR_INFO cursorInfo;
-    cursorInfo.dwSize = 1;
-    cursorInfo.bVisible = FALSE;
+    CONSOLE_CURSOR_INFO cursorInfo = {100, FALSE};
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
 }
 
 void drawBorder() {
-    int i;
     setColor(7);
     for (i = 0; i <= HEIGHT; i++) {
         gotoxy(0, i); printf("|");
@@ -76,133 +78,158 @@ int getLaneX(int lane) {
 
 void drawCar() {
     int x = getLaneX(carLane);
-    int i;
     setColor(shield ? 11 : 10);
-    for (i = 0; i < CAR_WIDTH; i++) {
-        gotoxy(x + i, HEIGHT - 2);
-        printf("A");
-    }
+    gotoxy(x, HEIGHT - 4); printf(" ^ ");
+    gotoxy(x, HEIGHT - 3); printf("/|\\");
+    gotoxy(x, HEIGHT - 2); printf("/ \\");
     setColor(7);
 }
 
 void clearCar() {
-    int lane, i;
-    for (lane = 0; lane < LANE_COUNT; lane++) {
-        int x = getLaneX(lane);
-        for (i = 0; i < CAR_WIDTH; i++) {
-            gotoxy(x + i, HEIGHT - 2);
-            printf(" ");
-        }
+    int x = getLaneX(carLane);
+    for (i = 0; i < 3; i++) {
+        gotoxy(x, HEIGHT - 4 + i);
+        printf("   ");
     }
 }
 
-void clearObstacles() {
-    int i, j;
-    for (i = 0; i < MAX_OBSTACLES; i++) {
-        if (obstacles[i].active) {
-            int x = getLaneX(obstacles[i].lane);
-            int yPos = (int)(obstacles[i].y);
-            int width = obstacles[i].boss ? BOSS_WIDTH : OBSTACLE_WIDTH;
-            for (j = 0; j < width; j++) {
-                gotoxy(x + j, yPos);
-                printf(" ");
+void drawLaneMarkers(int tick) {
+    int laneX;
+    setColor(8);
+    for (i = 1; i < HEIGHT; i++) {
+        if ((i + tick / 2) % 5 == 0) {
+            for (l = 0; l < LANE_COUNT - 1; l++) {
+                laneX = (l + 2) * LANE_WIDTH;
+                gotoxy(laneX, i);
+                printf("|");
             }
         }
+    }
+    setColor(7);
+}
+
+void addObstacle(int lane, bool boss) {
+    ObstacleNode* newObs = (ObstacleNode*)malloc(sizeof(ObstacleNode));
+    newObs->lane = lane;
+    newObs->y = 0;
+    newObs->boss = boss;
+    newObs->next = obstacleHead;
+    obstacleHead = newObs;
+}
+
+void clearObstacles() {
+    ObstacleNode* current = obstacleHead;
+    while (current != NULL) {
+        int x = getLaneX(current->lane);
+        int yPos = (int)(current->y);
+        int width = current->boss ? BOSS_WIDTH : OBSTACLE_WIDTH;
+        for (j = 0; j < width; j++) {
+            gotoxy(x + j, yPos);
+            printf(" ");
+        }
+        current = current->next;
     }
 }
 
 void drawObstacles() {
-    int i, j;
-    for (i = 0; i < MAX_OBSTACLES; i++) {
-        if (obstacles[i].active) {
-            int x = getLaneX(obstacles[i].lane);
-            int yPos = (int)(obstacles[i].y);
-            int width = obstacles[i].boss ? BOSS_WIDTH : OBSTACLE_WIDTH;
-
-            setColor(obstacles[i].boss ? 12 : 14);
-            for (j = 0; j < width; j++) {
-                gotoxy(x + j, yPos);
-                printf("%c", obstacles[i].boss ? '#' : 'X');
-            }
+    ObstacleNode* current = obstacleHead;
+    while (current != NULL) {
+        int x = getLaneX(current->lane);
+        int yPos = (int)(current->y);
+        int width = current->boss ? BOSS_WIDTH : OBSTACLE_WIDTH;
+        setColor(current->boss ? 12 : 14);
+        for (j = 0; j < width; j++) {
+            gotoxy(x + j, yPos);
+            printf("%c", current->boss ? '#' : 'X');
         }
+        current = current->next;
     }
     setColor(7);
 }
 
 void explosionEffect(int x, int y) {
-    int i;
     setColor(12);
     for (i = 0; i < 3; i++) {
-        gotoxy(x, y);     printf("* * *");
-        gotoxy(x, y + 1); printf(" * * ");
-        gotoxy(x, y + 2); printf("* * *");
+        gotoxy(x - 1, y);     printf(" * ");
+        gotoxy(x - 1, y + 1); printf("*X*");
+        gotoxy(x - 1, y + 2); printf(" * ");
         Sleep(100);
-        gotoxy(x, y);     printf("       ");
-        gotoxy(x, y + 1); printf("       ");
-        gotoxy(x, y + 2); printf("       ");
+        gotoxy(x - 1, y);     printf("   ");
+        gotoxy(x - 1, y + 1); printf("   ");
+        gotoxy(x - 1, y + 2); printf("   ");
         Sleep(100);
     }
     setColor(7);
 }
 
 void updateObstacles() {
-    int i;
+    ObstacleNode *current = obstacleHead, *prev = NULL;
+
     clearObstacles();
 
-    for (i = 0; i < MAX_OBSTACLES; i++) {
-        if (obstacles[i].active) {
-            obstacles[i].y += slowmo ? 0.2 : 0.5;
+    while (current != NULL) {
+        current->y += slowmo ? 0.2 : 0.5;
 
-            if (obstacles[i].y >= HEIGHT - 1) {
-                obstacles[i].active = false;
-                score++;
+        int carX = getLaneX(carLane);
+        int obsX = getLaneX(current->lane);
+        int obsWidth = current->boss ? BOSS_WIDTH : OBSTACLE_WIDTH;
 
-                level = score / 10 + 1;
-                speed = BASE_SPEED - (level - 1) * 5;
-                if (speed < MIN_SPEED) speed = MIN_SPEED;
+        bool xOverlap = (carX < obsX + obsWidth) && (carX + CAR_WIDTH > obsX);
+        bool yOverlap = ((int)(current->y) >= HEIGHT - 4 && (int)(current->y) <= HEIGHT - 2);
+
+        if (xOverlap && yOverlap) {
+            if (shield) {
+                shield = false;
+            } else {
+                explosionEffect(carX, HEIGHT - 4);
+                lives--;
+                if (lives <= 0) gameOver = true;
             }
-
-            int carX = getLaneX(carLane);
-            int obsX = getLaneX(obstacles[i].lane);
-            int obsWidth = obstacles[i].boss ? BOSS_WIDTH : OBSTACLE_WIDTH;
-
-            bool xOverlap = (carX < obsX + obsWidth) && (carX + CAR_WIDTH > obsX);
-            bool yOverlap = ((int)(obstacles[i].y) == HEIGHT - 2);
-
-            if (xOverlap && yOverlap) {
-                obstacles[i].active = false;
-
-                if (shield) {
-                    shield = false;
-                } else {
-                    explosionEffect(carX, HEIGHT - 3);
-                    lives--;
-                    if (lives <= 0) {
-                        gameOver = true;
-                    }
-                }
+            if (prev == NULL) {
+                obstacleHead = current->next;
+                free(current);
+                current = obstacleHead;
+            } else {
+                prev->next = current->next;
+                free(current);
+                current = prev->next;
             }
+            continue;
         }
+
+        if ((int)(current->y) > HEIGHT) {
+            score++;
+            if (prev == NULL) {
+                obstacleHead = current->next;
+                free(current);
+                current = obstacleHead;
+            } else {
+                prev->next = current->next;
+                free(current);
+                current = prev->next;
+            }
+            continue;
+        }
+
+        prev = current;
+        current = current->next;
     }
+
+    level = score / 10 + 1;
+    speed = BASE_SPEED - (level - 1) * 5;
+    if (speed < MIN_SPEED) speed = MIN_SPEED;
 }
 
 void spawnObstacle() {
-    int i;
-    for (i = 0; i < MAX_OBSTACLES; i++) {
-        if (!obstacles[i].active) {
-            obstacles[i].active = true;
-            obstacles[i].y = 0;
-            obstacles[i].lane = rand() % LANE_COUNT;
-            obstacles[i].boss = (level % 5 == 0 && rand() % 3 == 0);
-            break;
-        }
-    }
+    bool boss = (level % 5 == 0 && rand() % 3 == 0);
+    int lane = rand() % LANE_COUNT;
+    addObstacle(lane, boss);
 }
 
 void drawUI() {
     gotoxy(WIDTH + 5, 2); setColor(15); printf("Score : %d", score);
     gotoxy(WIDTH + 5, 3); printf("Level : %d", level);
-    gotoxy(WIDTH + 5, 4); printf("Lives : %d", lives);
+    gotoxy(WIDTH + 5, 4); setColor(lives == 1 ? 12 : 15); printf("Lives : %d", lives);
     gotoxy(WIDTH + 5, 5); printf("High  : %d", highScore);
     gotoxy(WIDTH + 5, 7); setColor(shield ? 11 : 8); printf("Shield : %s", shield ? "ON " : "OFF");
     gotoxy(WIDTH + 5, 8); setColor(slowmo ? 13 : 8); printf("SlowMo : %s", slowmo ? "ON " : "OFF");
@@ -213,7 +240,6 @@ void drawUI() {
 }
 
 void levelUpEffect() {
-    int i;
     setColor(10);
     for (i = 0; i < 3; i++) {
         gotoxy(WIDTH / 2 - 5, HEIGHT / 2);
@@ -228,8 +254,8 @@ void levelUpEffect() {
 
 void saveHighScore() {
     if (score > highScore) {
-        FILE *file = fopen("highscore.txt", "w");
-        if (file != NULL) {
+        FILE* file = fopen("highscore.txt", "w");
+        if (file) {
             fprintf(file, "%d", score);
             fclose(file);
         }
@@ -237,8 +263,8 @@ void saveHighScore() {
 }
 
 void loadHighScore() {
-    FILE *file = fopen("highscore.txt", "r");
-    if (file != NULL) {
+    FILE* file = fopen("highscore.txt", "r");
+    if (file) {
         fscanf(file, "%d", &highScore);
         fclose(file);
     }
@@ -246,9 +272,8 @@ void loadHighScore() {
 
 void activatePowerUp() {
     int power = rand() % 2;
-    if (power == 0) {
-        shield = true;
-    } else {
+    if (power == 0) shield = true;
+    else {
         slowmo = true;
         slowmoTicks = SLOWMO_DURATION;
     }
@@ -257,29 +282,33 @@ void activatePowerUp() {
 void gameOverScreen() {
     system("cls");
     setColor(12);
-    gotoxy(WIDTH / 2 - 5, HEIGHT / 2 - 2);
-    printf(" GAME OVER! ");
-    gotoxy(WIDTH / 2 - 7, HEIGHT / 2);
-    printf("Score: %d", score);
-    gotoxy(WIDTH / 2 - 10, HEIGHT / 2 + 2);
-    printf("Press R to Restart or X to Exit");
+    gotoxy(WIDTH / 2 - 6, HEIGHT / 2 - 2); printf("GAME OVER!");
+    gotoxy(WIDTH / 2 - 8, HEIGHT / 2); printf("Score     : %d", score);
+    gotoxy(WIDTH / 2 - 8, HEIGHT / 2 + 1); printf("High Score: %d", highScore);
+    gotoxy(WIDTH / 2 - 12, HEIGHT / 2 + 3); printf("Press 'R' to Restart or 'X' to Exit");
+    setColor(7);
 
-    char input;
+    char choice;
     while (1) {
-        if (_kbhit()) {
-            input = _getch();
-            if (input == 'r' || input == 'R') {
-                restartGame();
-                break;
-            } else if (input == 'x' || input == 'X') {
-                exit(0);
-            }
+        choice = _getch();
+        if (choice == 'r' || choice == 'R') {
+            restartGame();
+            break;
+        } else if (choice == 'x' || choice == 'X') {
+            exit(0);
         }
-        Sleep(100);
     }
 }
 
 void restartGame() {
+    ObstacleNode* current = obstacleHead;
+    while (current != NULL) {
+        ObstacleNode* temp = current;
+        current = current->next;
+        free(temp);
+    }
+    obstacleHead = NULL;
+
     score = 0;
     carLane = 1;
     gameOver = false;
@@ -290,10 +319,6 @@ void restartGame() {
     slowmo = false;
     slowmoTicks = 0;
 
-    for ( i = 0; i < MAX_OBSTACLES; i++) {
-        obstacles[i].active = false;
-    }
-
     system("cls");
     gameLoop();
 }
@@ -303,8 +328,8 @@ void gameLoop() {
     int lastLevel = level;
 
     hideCursor();
-    drawBorder();
     loadHighScore();
+    drawBorder();
 
     while (!gameOver) {
         clearCar();
@@ -325,10 +350,8 @@ void gameLoop() {
         if (tick % 2 == 0) {
             updateObstacles();
 
-            int spawnChance = 5 - level;
-            if (spawnChance < 1) spawnChance = 1;
-
-            if (rand() % spawnChance == 0) {
+            int spawnRate = (5 - level < 1) ? 1 : 5 - level;
+            if (rand() % spawnRate == 0) {
                 spawnObstacle();
             }
         }
@@ -338,16 +361,14 @@ void gameLoop() {
             lastLevel = level;
         }
 
-        if (slowmo) {
-            slowmoTicks--;
-            if (slowmoTicks <= 0) {
-                slowmo = false;
-            }
+        if (slowmo && --slowmoTicks <= 0) {
+            slowmo = false;
         }
 
+        drawLaneMarkers(tick);
+        drawUI();
         drawCar();
         drawObstacles();
-        drawUI();
 
         Sleep(speed);
         tick++;
@@ -359,10 +380,8 @@ void gameLoop() {
 
 int main() {
     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    srand(time(NULL));
+    srand((unsigned int)time(NULL));
     system("cls");
-
     gameLoop();
-
     return 0;
 }
